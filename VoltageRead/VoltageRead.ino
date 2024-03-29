e4acd13d9dada4b31505f809af5da594a9cec351
UnCommonSense001 <67968551+UnCommonSense001@users.noreply.github.com>
2024-03-28 23:35:09 -0400

    Matrix send on press only working

--- VoltageRead/VoltageRead.ino
+++ VoltageRead/VoltageRead.ino
@@ -2,7 +2,8 @@ using namespace std;
 
 #define voltageReadPin A0
 
-bool buttonFlag = false;
+// bool buttonFlag = false;
+bool colFlags[] = {false, false, false, false};
 int columnPins[] = {16, 4, 12, 5};
 String matrix[4][4] = {
   {"1", "2", "3", "A"},
@@ -20,49 +21,34 @@ void setup() {
     }
 }
 
+int readCol(int colPin) {
+  digitalWrite(colPin, LOW);
+  int rawReading = analogRead(voltageReadPin);
+  float voltage = rawReading * (3.3 / 1023.0);
+  digitalWrite(colPin, HIGH);
+
+  if (voltage < 0.4)
+    return 0;
+  if (voltage < 1.2)
+    return 1;
+  if (voltage < 2.0)
+    return 2;
+  if (voltage < 3)
+    return 3;
+  return -1;
+}
+
 String readMatrix() {
   for (int col=0; col<sizeof columnPins/sizeof columnPins[0]; col++) {
     int colPin = columnPins[col];
-    digitalWrite(colPin, LOW);
-    int rawReading = analogRead(voltageReadPin);
-    float voltage = rawReading * (3.3 / 1023.0);
-    digitalWrite(colPin, HIGH);
-    if(voltage <= 3 && !buttonFlag)
-    {
-      buttonFlag = true;
-      if (voltage < 0.4)
-        return matrix[0][col];
-      if (voltage < 1.2)
-        return matrix[1][col];
-      if (voltage < 2.0)
-        return matrix[2][col];
+    int row = readCol(colPin);
+    if(row != -1 && !colFlags[col]) {
+      colFlags[col] = true;
+      return matrix[row][col];
     }
-    if(voltage > 3)
-    {
-      buttonFlag = false;
-    }
-    // if (voltage < 0.4)
-    //   if(!buttonFlag) {
-    //     buttonFlag = true;
-    //     return matrix[0][col];
-    //   }
-    // if (voltage < 1.2)
-    //   if(!buttonFlag) {
-    //     buttonFlag = true;
-    //     return matrix[1][col];
-    //   }
-    // if (voltage < 2.0)
-    //   if(!buttonFlag) {
-    //     buttonFlag = true;
-    //     return matrix[2][col];
-    //   }
-    // if (voltage < 3.0)
-    //   if(!buttonFlag) {
-    //     buttonFlag = true;
-    //     return matrix[3][col];
-    //   }
+    if(row == -1 && colFlags[col])
+      colFlags[col] = false;
   }
-  // buttonFlag = false;
   return "";
 }
 
@@ -70,9 +56,4 @@ void loop() {
     String curVal = readMatrix();
     if(!curVal.equals(""))
       Serial.println(curVal);
-    // Serial.println("\t buttonFlag: " + buttonFlag);
-    // rawReading = analogRead(voltageReadPin);
-    // voltage = rawReading * (3.3 / 1023.0);
-    // Serial.print("Voltage: ");
-    // Serial.println(voltage);
 }
