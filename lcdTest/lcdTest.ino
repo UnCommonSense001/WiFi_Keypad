be17fc241f3935e2f25e248457c3a86bb5a0c4ac
UnCommonSense001 <67968551+UnCommonSense001@users.noreply.github.com>
2024-04-04 18:27:37 -0400

    add pinouts to LCD code

--- lcdTest/lcdTest.ino
+++ lcdTest/lcdTest.ino
@@ -1,5 +1,11 @@
 #include <LiquidCrystal_I2C.h>
 
+/* PINOUT
+5V - VCC
+G - GND
+D2 - SDA
+D1 - SCL
+*/
 LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
@@ -10,8 +16,8 @@ void setup()
   delay(5000);
   lcd.setCursor(0,0);
   lcd.print("Reminder to fix");
-  lcd.setCursor(2, 1); // x, y
-  lcd.print("my contrast!");
+  lcd.setCursor(1, 1); // x, y
+  lcd.print("the contrast!");
 }
 
 
