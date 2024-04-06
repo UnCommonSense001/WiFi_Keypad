f72ae80cefc3ba9a600b8252d011b81a96053405
UnCommonSense001 <67968551+UnCommonSense001@users.noreply.github.com>
2024-04-06 18:34:14 -0400

    switch to char arrays

--- VoltageRead/VoltageRead.ino
+++ VoltageRead/VoltageRead.ino
@@ -7,26 +7,25 @@ looking at my button matrix with pins on the left
 D1
 D6
 D2
-D0
+D0uuu
 Then rest of pins to breadboard (for voltage ladder)
 */
 
 bool colFlags[] = {false, false, false, false};
-int columnPins[] = {16, 4, 12, 5};
-String matrix[4][4] = {
-  {"1", "2", "3", "A"},
-  {"4", "5", "6", "B"},
-  {"7", "8", "9", "C"},
-  {"*", "0", "#", "D"}
-};
+int columnPins[] = { 16, 4, 12, 5 };
+char row0[4] = { '1', '2', '3', 'A' };
+char row1[4] = {'4', '5', '6', 'B' };
+char row2[4] = {'7', '8', '9', 'C' };
+char row3[4] = {'*', '0', '#', 'D' };
+char* matrix[4] = {row0, row1, row2, row3};
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(voltageReadPin, INPUT);
-    for(int colPin: columnPins) {
-      pinMode(colPin, OUTPUT);
-      digitalWrite(colPin, HIGH);
-    }
+  Serial.begin(115200);
+  pinMode(voltageReadPin, INPUT);
+  for (int colPin : columnPins) {
+    pinMode(colPin, OUTPUT);
+    digitalWrite(colPin, HIGH);
+  }
 }
 
 int readCol(int colPin) {
@@ -46,22 +45,22 @@ int readCol(int colPin) {
   return -1;
 }
 
-String readMatrix() {
-  for (int col=0; col<sizeof columnPins/sizeof columnPins[0]; col++) {
+char readMatrix() {
+  for (int col = 0; col < sizeof row0 / sizeof row0[0]; col++) {
     int colPin = columnPins[col];
     int row = readCol(colPin);
-    if(row != -1 && !colFlags[col]) {
+    if (row != -1 && !colFlags[col]) {
       colFlags[col] = true;
       return matrix[row][col];
     }
-    if(row == -1 && colFlags[col])
+    if (row == -1 && colFlags[col])
       colFlags[col] = false;
   }
-  return "";
+  return '\0';
 }
 
 void loop() {
-    String curVal = readMatrix();
-    if(!curVal.equals(""))
-      Serial.println(curVal);
+  char curVal = readMatrix();
+  if (!curVal == '\0')
+    Serial.println(curVal);
 }
