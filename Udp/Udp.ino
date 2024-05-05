bd528e4472ab6c45ff3ad8346620d1b85c1cdc8d
UnCommonSense001 <67968551+UnCommonSense001@users.noreply.github.com>
2024-05-05 12:23:45 -0400

    WORKING matrix to LCD unidirectional with clear and two line - sending one char at a time

--- Udp/Udp.ino
+++ Udp/Udp.ino
@@ -57,10 +57,11 @@ bool colFlags[] = {false, false, false, false};
 int columnPins[] = {16, 4, 12, 5}; // actual pins
 // int columnPins[] = {D0, D2, D6, D1}; // attempt at converting (NOT TESTED)
 
-char row0[4] = { '1', '2', '3', 'A' };
-char row1[4] = {'4', '5', '6', 'B' };
-char row2[4] = {'7', '8', '9', 'C' };
-char row3[4] = {'*', '0', '#', 'D' };
+char row0[4] = {'1', '2', '3', 'A'};
+char row1[4] = {'4', '5', '6', 'B'};
+char row2[4] = {'7', '8', '9', 'C'};
+// char row3[4] = {'*', '0', '#', 'D' };
+char row3[4] = {'*', '0', '#', '!'};
 char* matrix[4] = {row0, row1, row2, row3};
 
 void sendPacket(const char* addresss, char msg) {
@@ -129,16 +130,16 @@ void setup() {
 
   delay(3000);
   Serial.println("sending startup 1!");
-  sendPacket(destination_IP, "startup 1!\r\n");
+  sendPacket(destination_IP, "startup 1!");
   delay(250);
   Serial.println("sending startup 2!!");
-  sendPacket(destination_IP, "startup 2!!\r\n");
+  sendPacket(destination_IP, "startup 2!!");
   delay(250);
   Serial.println("sending startup 3!!!");
-  sendPacket(destination_IP, "startup 3!!!\r\n");
+  sendPacket(destination_IP, "startup 3!!!");
   delay(250);
   Serial.println("sending startup 4!!!!");
-  sendPacket(destination_IP, "startup 4!!!!\r\n");
+  sendPacket(destination_IP, "startup 4!!!!");
   Serial.println("ALL 4 STARTUPS SENT");
   delay(250);
 }
--- Udp2/Udp2.ino
+++ Udp2/Udp2.ino
@@ -13,6 +13,14 @@
 
   adapted from Ethernet library examples
 */
+
+/* PINOUT for LCD
+5V - VCC
+G - GND
+D2 - SDA
+D1 - SCL
+*/
+
 using namespace std;
 
 #include <ESP8266WiFi.h>
@@ -21,16 +29,7 @@ using namespace std;
 #include <iostream>
 #include <WiFiCredentials.h>
 
-// #ifndef STASSID
-// #define STASSID "wemosD1Mini"
-// #define STAPSK "wemosD1Mini"
-// #endif
-
-const int buttonPin = 16; // D0
-int buttonState = 0;
-bool buttonFlag = false;
-
-const int ledPin = 5; // D1, 220ohm resistor
+#include <LiquidCrystal_I2C.h>
 
 const char* ssid = SSID;
 const char* password = PSK;
@@ -56,7 +55,9 @@ char pressed[] = "pressed\r\n";
 char released[] = "released\r\n";
 
 WiFiUDP Udp;
-  
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
 void sendPacket(const char* addresss, string msg) {
   Udp.beginPacket(addresss, 8888);
   Udp.write(msg.c_str(), msg.size());
@@ -65,6 +66,13 @@ void sendPacket(const char* addresss, string msg) {
 
 void setup() {
   Serial.begin(115200);
+
+  lcd.init(); // initialize the lcd
+  lcd.backlight();
+  delay(1000);
+  lcd.setCursor(0, 0);
+  lcd.clear();
+
   WiFi.mode(WIFI_STA);
   WiFi.setHostname(hostname);
   WiFi.config(myIP, apIP, IPAddress(255, 255, 255, 0));
@@ -77,26 +85,26 @@ void setup() {
   Serial.println(WiFi.localIP());
   Serial.printf("UDP server on port %d\n", localPort);
   Udp.begin(localPort);
-
-  pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
 }
 
 // unsigned long startTime = millis();
 
+int x = 0;
+int y = 0;
+
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  if(buttonState == HIGH && !buttonFlag)
-  {
-    sendPacket(destination_IP, "pressed\r\n");
-    buttonFlag = true;
-  } else {
-    if(buttonState == LOW && buttonFlag)
-    {
-      sendPacket(destination_IP, "released\r\n");
-      buttonFlag = false;
-    }
-  }
+  // buttonState = digitalRead(buttonPin);
+  // if(buttonState == HIGH && !buttonFlag)
+  // {
+  //   sendPacket(destination_IP, "pressed\r\n");
+  //   buttonFlag = true;
+  // } else {
+  //   if(buttonState == LOW && buttonFlag)
+  //   {
+  //     sendPacket(destination_IP, "released\r\n");
+  //     buttonFlag = false;
+  //   }
+  // }
 
   // if there's data available, read a packet
   int packetSize = Udp.parsePacket();
@@ -104,32 +112,57 @@ void loop() {
     // read the packet into packetBufffer
     int n = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
     packetBuffer[n] = 0;
+    if(n == 1 && packetBuffer[0] == '!')
+    {
+      Serial.println("CLEAR");
+      lcd.clear();
+      x = 0;
+      y = 0;
+    } else { // CHANGE so makes the x and y in range FIRST then displays (clear if necessary (if off screen on second line))
+      Serial.println(packetBuffer);
+      if(x > 15 && y == 0)
+      {
+        x = 0;
+        y = 1;
+      }
+      if(x > 15 && y == 1)
+      {
+        x = 0;
+        y = 0;
+        lcd.clear();
+      }
+      lcd.setCursor(x, y);
+      lcd.print(packetBuffer);
+      x++;      
+      delay(50);
+    }
+
     // Serial.println("Contents:");
     // Serial.println(packetBuffer);
 
-    if(memcmp(&ack, &packetBuffer, sizeof(ack)/sizeof(char)) == 0)
-    {
-      Serial.println("MEMCMP ACK");
-    } else {
-      if(memcmp(&pressed, &packetBuffer, sizeof(pressed)/sizeof(char)) == 0)
-      {
-        Serial.println("LED ON");
-        digitalWrite(ledPin, HIGH);
-      } else {
-        if(memcmp(&released, & packetBuffer, sizeof(released)/sizeof(char)) == 0)
-        {
-          Serial.println("LED OFF");
-          digitalWrite(ledPin, LOW);
-        } else {
-          Serial.printf("Received packet of size %d from %s:%d\n    (to %s:%d, free heap = %d B)\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort(), Udp.destinationIP().toString().c_str(), Udp.localPort(), ESP.getFreeHeap());
-          Serial.println("Contents:");
-          Serial.println(packetBuffer);
-        }
-      }
-      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-      Udp.write(ReplyBuffer);
-      Udp.endPacket();
-    }
+    // if(memcmp(&ack, &packetBuffer, sizeof(ack)/sizeof(char)) == 0)
+    // {
+    //   Serial.println("MEMCMP ACK");
+    // } else {
+    //   if(memcmp(&pressed, &packetBuffer, sizeof(pressed)/sizeof(char)) == 0)
+    //   {
+    //     Serial.println("LED ON");
+    //     digitalWrite(ledPin, HIGH);
+    //   } else {
+    //     if(memcmp(&released, & packetBuffer, sizeof(released)/sizeof(char)) == 0)
+    //     {
+    //       Serial.println("LED OFF");
+    //       digitalWrite(ledPin, LOW);
+    //     } else {
+    //       Serial.printf("Received packet of size %d from %s:%d\n    (to %s:%d, free heap = %d B)\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort(), Udp.destinationIP().toString().c_str(), Udp.localPort(), ESP.getFreeHeap());
+    //       Serial.println("Contents:");
+    //       Serial.println(packetBuffer);
+    //     }
+    //   }
+    //   Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    //   Udp.write(ReplyBuffer);
+    //   Udp.endPacket();
+    // }
   }
 }
 
