29ec21b0e4a9dda722e4e5271bdff9e8da7a048f
UnCommonSense001 <67968551+UnCommonSense001@users.noreply.github.com>
2024-06-19 11:35:28 -0400

    some good stuff - double press sort of working, still need to figure out the non-letter keys but good progress

--- matrixReadDoublePress/matrixReadDoublePress.ino
+++ matrixReadDoublePress/matrixReadDoublePress.ino
@@ -34,38 +34,58 @@ int columnPins[] = {16, 4, 12, 5}; // actual pins
 // int columnPins[] = {D0, D2, D6, D1}; // attempt at converting (NOT TESTED)
 
 int baseNum = 65; // ASCII "A"
+int maxNum = 90; // ASCII "Z"
 
 //WITH TIME 
 struct button_t {
   int col;
   int row;
-  bool shifted;
   unsigned long time;
+  char character;
 };
 
 button_t lastButton;
 
 char msgToSend[40];
 
-void setButton(button_t &b, int bCol, int bRow, bool bShifted, unsigned long &bTime) {
+void setButton(button_t &b, int bCol, int bRow, unsigned long &bTime, char bCharacter) {
   b.col = bCol;
   b.row = bRow;
-  b.shifted = bShifted;
   b.time = bTime;
+  b.character = bCharacter;
 }
 
 void setButton(button_t &bDest, button_t &bOrigin) {
   bDest.col = bOrigin.col;
   bDest.row = bOrigin.row;
-  bDest.shifted = bOrigin.shifted;
   bDest.time = bOrigin.time;
+  bDest.character = bOrigin.character;
 }
 
-bool compareRowsAndCols(button_t &b1, button_t &b2) {
-  if(b1.col == b2.col && b1.row == b2.row) {
+// bool compareRowsAndCols(button_t &b1, button_t &b2) {
+//   if(b1.col == b2.col && b1.row == b2.row) {
+//     return true;
+//   }
+//   return false;
+// }
+
+bool compareButtons(button_t &b1, button_t &b2) {
+  if(b1.character == b2.character) {
     return true;
   }
   return false;
+}
+
+bool isValid(button_t &b1) {
+  if(b1.character >= baseNum && b1.character <= maxNum) {
+    return true;
+  }
+  return false;
+}
+
+void clearButton(button_t &b1) {
+  unsigned long time = millis();
+  setButton(b1, -1, -1, time, 0);
 }
 
 int readCol(int colPin) {
@@ -94,114 +114,16 @@ void readMatrix(button_t &lButton) {
 
       unsigned long curTime = millis();
       button_t curButton;
-      setButton(curButton, col, row, false, curTime);
-
-      char unshiftedChar = baseNum + (curButton.row * 8) + (curButton.col * 2);
+      char unshiftedChar = baseNum + (row * 8) + (col * 2);
       char shiftedChar = unshiftedChar + 1;
+      setButton(curButton, col, row, curTime, unshiftedChar);
 
-      Serial.println(unshiftedChar);
-      Serial.println(shiftedChar);
-
-      if(compareRowsAndCols(lButton, curButton) && curTime - lButton.time <= 2000) {
-        Serial.println("DBL");
+      if(compareButtons(lButton, curButton) && isValid(curButton) && curButton.time - lButton.time <= 1500) {
+        Serial.println(shiftedChar);
+        clearButton(lButton);
+      } else {
+        setButton(lButton, curButton);
       }
-
-      setButton(lButton, curButton);
-      
-      // char lastChar = baseNum + (lButton.row * 8) + (lButton.col * 2);
-
-      // if(lButton.row == )
-
-
-      // if(last[0] == col && last[1] == row && millis() - lTime <= COOLDOWN_MILLIS) {
-      //   expTime = millis();
-      //   int charInt = baseNum + (row * 8) + 1 + (col * 2)
-      //   if(charInt <= 90) {
-      //     Serial.println(char(charInt);
-      //     last[0] = -1;
-      //     last[1] = -1;
-      //     lChar = 0;
-      //     // double press (BOTH LETTERS)
-      //   }
-      // } else {
-      //   // single press
-      //   int charInt = baseNum + (row * 8) + (col * 2)
-      //   if(charInt <= 90) {
-      //     // letter
-      //     Serial.println(char(baseNum + (row * 8) + (col * 2)));
-      //   }
-      // }
-
-
-      // if(unshiftedChar == lastChar) {
-      //   Serial.println(shiftedChar);
-      //   last[0] = -1;
-      //   last[1] = -1;
-      //   lTime = millis();
-      // } else {
-      //   if 
-      // }
-
-      // if(col == last[0] and row == last[1] && millis() - lTime <= 2000) {
-      //   // double press in amount of time
-      //   Serial.println(shiftedChar);
-      //   last[0] = -1;
-      //   last[1] = -1;
-      //   lTime = millis();
-      // } else if (col != last[0] || row != last[1] || (millis() - lTime > 2000)) {
-      //   // double press in longer than amount of time or a different button
-      //   // should reset
-      //   Serial.println(unshiftedChar);
-      //   last[0] = col;
-      //   last[1] = row;
-      //   lTime = millis();
-      // }
-
-      // if(col == last[0] and row == last[1]) {
-      //   // double press over however long
-      //   if(millis() - lTime <= 2000) {
-      //     // double press within 2 sec
-      //     System.out.println(char(charInt + 1));
-      //   }
-      //   last[0] = -1;
-      //   last[1] = -1;
-      //   lTime = millis();
-      // }
-
-      // last[0] = col;
-      // last[1] = row;
-      // lTime = millis();
-
-
-
-      // // Serial.println(millis() - lTime);
-      // if(millis() - lTime <= COOLDOWN_MILLIS && last[0] == col && last[1] == row) {
-      //   Serial.println("DBL");
-      //   if((baseNum + (row * 8) + 1 + (col * 2)) <= 90) {
-      //     char toReturn = (baseNum + (row * 8) + 1 + (col * 2));
-      //     Serial.print("--: ");
-      //     Serial.println(toReturn);
-      //     strcat(msgToSend, &toReturn);
-
-      //   }
-      // }
-      // if(toReturn != -1) {
-        // char charToReturn = char(toReturn);
-        // Serial.println(charToReturn);
-        // strcat(msgToSend, &charToReturn);
-      // }
-    //   lTime = millis();
-    //   last[0] = col;
-    //   last[1] = row;
-    //   if ((baseNum + (row * 8) + (col * 2)) <= 90) {
-    //     // return (baseNum + (row * 8) + (col * 2));
-    //     char charToReturn = char(baseNum + (row * 8) + (col * 2));
-    //     Serial.println(charToReturn);
-    //     strcat(msgToSend, &charToReturn);
-    //   } else {
-    //     return 0;
-    //   }
-    // }
     }
     if (row == -1 && colFlags[col])
       colFlags[col] = false;
@@ -221,12 +143,10 @@ void setup() {
 
 void loop() {
   readMatrix(lastButton);
-  // Serial.println(msgToSend);
+  unsigned long curTime = millis();
+  if(isValid(lastButton) && curTime - lastButton.time >= 1500) {
+    Serial.println(lastButton.character);
+    clearButton(lastButton);
+  }
   delay(100);
-  // if(curVal != '\0')
-  // {
-  //   Serial.println(curVal);
-  //   Serial.println(msgToSend);
-  //   delay(100);
-  // }
 }
