56da7fb58bc070c87c803f8c9a7c6154bb22dce6
UnCommonSense001 <67968551+UnCommonSense001@users.noreply.github.com>
2024-06-21 13:21:05 -0400

    added basic display code

---
+++ displayUDP/displayUDP.ino
@@ -0,0 +1,78 @@
+/*
+ === LCD PINOUT ===
+  5V - VCC
+  G - GND
+  D2 - SDA
+  D1 - SCL
+*/
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <string.h>
+// #include <iostream>
+#include <LiquidCrystal_I2C.h>
+#include <WiFiCredentials.h>
+
+using namespace std;
+
+const char* ssid = SSID;
+const char* password = PSK;
+
+IPAddress apIP(192, 168, 1, 1);
+IPAddress myIP(192, 168, 1, 3);
+
+const char* hostname = "wemi-b";
+const char* destination_IP = "192.168.1.2";
+unsigned int localPort = 8888;
+
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1];
+
+WiFiUDP Udp;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// const int MAX_MESSAGE_SIZE = UDP_TX_PACKET_MAX_SIZE;
+
+void setup() {
+  Serial.begin(115200);
+
+  lcd.init();
+  lcd.backlight();
+  delay(1000);
+  lcd.setCursor(0, 0);
+  lcd.clear();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.setHostname(hostname);
+  WiFi.config(myIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.begin(ssid, password);
+  bool wifiFlag = false;
+  while (WiFi.status() != WL_CONNECTED) {
+    if(!wifiFlag) {
+      lcd.print("Connecting...");
+      wifiFlag = true;
+    }
+    Serial.print('.');
+    delay(500);
+  }
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.printf("UDP server on port %d\n", localPort);
+  Udp.begin(localPort);
+}
+
+void loop() {
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    // read the packet into packetBufffer
+    int n = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    packetBuffer[n] = 0;
+    Serial.println(packetBuffer);
+    lcd.setCursor(0, 0);
+    lcd.clear();
+    lcd.print(packetBuffer);
+  }
+}
